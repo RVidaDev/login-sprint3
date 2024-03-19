f15d752836f74b813b956ef07ca1afcfab317398
RVidaDev <vidraf9@gmail.com>
2024-03-19 16:59:16 -0300

    mudanças no template py -implementação pandas

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import csv
+import pandas as pd
 
-def ler_csv(file):
+# Função para ler o CSV e converter em uma lista de dicionários
+def csv_to_list_of_dicts(file_path):
     try:
-        with open(arquivo_csv, 'r') as arquivo:
-            leitor = csv.DictReader(arquivo)
-            dados = [dict(linha) for linha in leitor_csv]
-        return dados
+        # Lê o arquivo CSV e converte em um DataFrame
+        df = pd.read_csv(file_path, error_bad_lines=False)
+        
+        # Converte o DataFrame em uma lista de dicionários
+        data_list = df.to_dict(orient='records')
+        
+        return data_list
     except FileNotFoundError:
-        print(f"O arquivo '{arquivo_csv}' não foi encontrado.")
+        print("Arquivo não encontrado.")
         return []
-    except Exception as e:
-        print(f"Ocorreu um erro ao ler o arquivo CSV: {e}")
+    except pd.errors.ParserError:
+        print("Erro ao analisar o arquivo CSV.")
         return []
 
-
-def main():
-   
-            
-if __name__ == "__main__":
-    main()
+# Exemplo de uso
+file_path = 'seu_arquivo.csv'
+data = csv_to_list_of_dicts(file_path)
+print(data)
